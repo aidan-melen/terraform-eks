bbc4914e7d65d82d226104891d0ebbd5dc95bbd1
Aidan Melen <aidan-melen@pluralsight.com>
2022-02-24 16:08:44 +0000

    fix variable name bug

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = var.name
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
